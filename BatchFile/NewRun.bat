0e541c1fe011c331884ccd5006188bae9e38fa45
abhikdixit <abhinay.dixit@hotmail.com>
2022-04-22 16:09:59 +0530

    Adding and Updating new file

--- BatchFile/NewRun.bat
+++ BatchFile/NewRun.bat
@@ -1,2 +1,2 @@
 cd C:\Users\adixit\PycharmProjects\RobotFramework\
-python.exe -m robot -d Results OrangeHRM/OrangeHRM_Logo_Click.robot
+python.exe -m robot -d Results OrangeHRM/SmartBear_Login.robot
